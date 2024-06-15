155087e04d0d88aba6c8887dbcc06798b7d0d623
matozinho <matozinhof.academico@gmail.com>
2024-06-15 19:49:05 -0300

    feat: add nix-shell
    Signed-off-by: matozinho <matozinhof.academico@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "E-commerce front-end flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [ nodejs corepack biome ];
+      };
+    });
+}
